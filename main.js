9f06e4fb57dc3655a3819faffd5949406779f37f
Joao Vitor Rodrigues <joaovitor@pop-os.localdomain>
2023-01-13 19:47:55 -0300

    estrutura do site sendo feita

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
+  document.getElementById("navigationMenu").style.width = "100%";
+  document.getElementById("section-2").style.height = "0vh";
+  document.getElementById("section-3").style.height = "0vh";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("navigationMenu").style.width = "0";
+  document.getElementById("section-2").style.height = "100vh";
+  document.getElementById("section-3").style.height = "100vh";
 }
